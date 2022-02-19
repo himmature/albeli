da39dc01394cdc54ada4e8e7a38aee94db02dd9f
Himanshu.Yawalkar <himanshu.yawalkar@msci.com>
2022-02-19 18:03:02 +0530

    fixed the image upload bug

--- app/js/templates/Index.js
+++ app/js/templates/Index.js
@@ -3,9 +3,9 @@ import {canvasControlsPrimary, canvasControlsSecondary} from "./CanvasControls";
 import {imageUploadSection} from "./ImageUploadSection";
 
 export const main = `<div class="main-header"><h1>Photo Editor - Created by Himanshu Yawalkar</h1></div>
-<form class='image-upload' action="#">
+<div class='image-upload'>
   ${imageUploadSection}
-</form>
+</div>
 <div class='canvas-container'>
     ${canvasControlsPrimary}
     ${mainCanvas}
